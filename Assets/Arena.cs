91c5f891e0da483b0add349042115fac45851ac1
Alex May <Alex May>
2016-05-15 23:10:26 +0100

    Made the Arena class dynamically reposition cameras and borders.

--- Assets/Arena.cs
+++ Assets/Arena.cs
@@ -5,10 +5,16 @@ using System.Collections;
 /// The Arena defines the playable space. It should be placed at the origin in the world. The size should not be altered at run time.
 /// The barriers in the field could be linked to this class to make changing its size easier either in the editor or in real time.
 /// </summary>
+/// 
+[ExecuteInEditMode()]
 public class Arena : MonoBehaviour {
 
     public float width = 456; // hard coded defaults as I wanted a low-resolution screen ;)
     public float height = 256;
+
+    public Camera mainCamera;
+    public GameObject[] borders;
+    public Camera[] cameras;
 	
 	public void OnDrawGizmos()
 	{
@@ -23,6 +29,24 @@ public class Arena : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        int i = 0;
+        for (int x = -1; x < 2; x++)
+        {
+            for (int y = -1; y < 2; y++)
+            {
+                if (x != 0 || y != 0)
+                {
+                    cameras[i].orthographicSize = mainCamera.orthographicSize;
+                    cameras[i].transform.localPosition = new Vector3(((float)x + 0.5f) * width, ((float)y + 0.5f) * height, -10);
+                    i++;
+                }
+                else
+                    mainCamera.transform.localPosition = new Vector3(width / 2, height / 2, -10);
+            }
+        }
+        borders[0].transform.localPosition = new Vector3(width * 0.5f, -250, 0);
+        borders[1].transform.localPosition = new Vector3(width * 0.5f, 250 + height, 0);
+        borders[2].transform.localPosition = new Vector3(-250, height * 0.5f, 0);
+        borders[3].transform.localPosition = new Vector3(250 + width, height * 0.5f, 0);
+    }
 }
